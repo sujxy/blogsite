593a29b85a2a2dafb9f59113d83f6e8a14703841
sujxy <sujay052003@gmail.com>
2023-03-15 16:37:27 +0530

    connected database to store posts

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require('mongoose') ; 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true}); 
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,25 +18,43 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
-var posts =[] ;
+//blog schema 
+
+const blogSchema = new mongoose.Schema({
+  title : {
+    type : String,
+    required : [true , 'please provide a title']
+  } ,
+  body : {
+    type : String ,
+    required : [true , 'body cannot be empty']
+  }
+}) ;
+
+const Blog = mongoose.model('Blog' , blogSchema) ;
  
 app.get("/",function(req,res){
+
+  async function getPosts() {
+    
+    const posts = await Blog.find({}) ;
+
+    res.render("home.ejs" , {homeContent : homeStartingContent , postArr : posts}) ;
+  }
+
+  getPosts() ; 
   
-  res.render("home.ejs" , {homeContent : homeStartingContent , postArr : posts}) ;
 }) ;
 
 app.get("/posts/:title" , function(req,res){
-  const reqTitle = req.params.title ;
+  const reqTitle = _.lowerCase(req.params.title) ;
   
-  posts.forEach(function(post) {
+  async function findPost() {
+    const reqPost = await Blog.find({title : reqTitle}) ; 
+    res.render("post.ejs" , {post_Title : reqPost[0].title , post_Content : reqPost[0].body}) ;
+  }
 
-    const currTitle = _.lowerCase(post.title) ; //using Lodash
-    const currBody = post.body ; 
-
-    if (currTitle === reqTitle) {
-      res.render("post.ejs" , {post_Title : post.title , post_Content : post.body}) ;
-    }
-  }) ; 
+  findPost() ;
 
 });
 
@@ -51,12 +71,26 @@ app.get("/compose" , function(req,res) {
 }) ;
 
 app.post("/compose" , function(req,res){
-  const post = {
+
+  const post = new Blog({
     title : req.body.postTitle ,
     body : req.body.postBody ,
-  }
-  posts.push(post) ; 
-  res.redirect("/")
+  }) ;
+
+  post.save() ;  
+  res.redirect("/") ;
+}) ;
+
+app.post('/delete' , (req,res) => {
+   const target = req.body.postName ;
+
+   async function deletePost( ) {
+       const result = await Blog.deleteOne({title : target}) ;
+
+       res.redirect('/') ;
+   } 
+
+   deletePost() ;
 })
 
 
